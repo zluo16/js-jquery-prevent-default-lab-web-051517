c1efc08526ef0f24cc720a9b5ec9b083f2093221
zluo16 <zluo@alumni.berklee.edu>
2017-07-01 18:55:25 -0400

    Done.

--- js/todo.js
+++ js/todo.js
@@ -1,5 +1,11 @@
 $(document).ready(function(){
-  // call functions here
+  submitForm()
 });
 
-// define functions here
+function submitForm() {
+  $('form').on('submit', function(event) {
+    var item = $('#item').val()
+    $('ol').append(`<li>${item}</li>`)
+    event.preventDefault()
+  })
+}
